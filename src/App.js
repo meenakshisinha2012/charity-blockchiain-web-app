cc7463fd21b65a02cdf3c6976950767c8a8bdbde
Meenakshi Sinha <meenakshisinha2012@gmail.com>
2021-03-07 18:51:21 +0530

    Metamask Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Web3 from "web3";
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { account: "" };
+  }
+
+  //Component life cycle method : that gets called before render()
+  componentWillMount() {
+    this.loadBlockchainData();
+  }
+
+  //to connect to metamask wallet
+  async loadBlockchainData() {
+    // "Web3.providers.givenProvider" will be set if in an Ethereum supported browser.
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8546");
+    const network = await web3.eth.net.getNetworkType();
+    await window.ethereum.enable();
+    console.log("Network: ", network);
+    const accounts = await web3.eth.getAccounts();
+    console.log("account Number : ", accounts);
+    this.setState({ account: accounts[0] });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello World!!</h1>
+        <p>Your account: {this.state.account}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
